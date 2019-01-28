8e8ad0a096cc0b690b538dc3feaf05dbf67cfe11
kein505 <kein1106@gmail.com>
2019-01-28 10:52:13 +0500

    Add files for test2

---
+++ Equat2/Equat2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Equat2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+            var c = double.Parse(Console.ReadLine());
+
+            var result = Solve(a, b, c);
+
+            Console.WriteLine(result[0]);
+            Console.WriteLine(result[1]);
+        }
+        public static double[] Solve(double a, double b, double c)
+        {
+            if (a == 0)
+            {
+                if (b == 0) return new double[0];
+                else return new double[] { -c / b };
+            }
+            var discriminant = b * b - 4 * a * c;
+            if (discriminant > 0)
+            {
+                var x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                var x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                return new[] { x1, x2 };
+            }
+            else if (discriminant == 0)
+                return new[] { -b / (2 * a) };
+
+            else return new double[0];
+
+        }
+    }
+}
